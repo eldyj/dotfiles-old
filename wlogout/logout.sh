b29a088de1bca59e72835ce7942ca1d5bd368574
Eldyj <lukassvaibodas@gmail.com>
2022-09-18 20:21:06 +0300

    switched to swaylock-effects

---
+++ wlogout/logout.sh
@@ -0,0 +1,12 @@
+swaylock \
+--screenshots \
+--clock \
+--indicator \
+--indicator-radius 75 \
+--indicator-thickness 6 \
+--effect-blur 8x2 \
+--ring-color 81a1c1 \
+--key-hl-color 81c1a1 \
+--line-color 00000000 \
+--inside-color 2e3440 \
+--separator-color 00000000
